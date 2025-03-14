993fec19621d599670386609147a3e802bbf7582
deriando <10672809+deriando@users.noreply.github.com>
2025-03-14 22:29:19 +0800

    test socketio

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,15 +1,24 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
-
+import cors from "cors";
+const PORT = 3001;
 const app = express();
 const httpServer = createServer(app);
+
+const corsOptions = {
+  origin: process.env.APP_BASE_URL, // Replace with your frontend's URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
 const io = new Server(httpServer, {
-  /* options */
+  cors: corsOptions,
 });
 
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
 });
 
-httpServer.listen(3001);
+console.log(`listening on *:${PORT}`);
+httpServer.listen(PORT);
