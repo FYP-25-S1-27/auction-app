eb75539fe227502d81825ea8f14415b9fa6aa684
deriando <10672809+deriando@users.noreply.github.com>
2025-03-14 21:36:22 +0800

    update db schema to match diagram + data seeding

--- apps/web/drizzle.config.ts
+++ apps/web/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle_output",
-  schema: "./lib/schema.ts",
+  schema: "./src/lib/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DB_CONNECTION_STRING!,
