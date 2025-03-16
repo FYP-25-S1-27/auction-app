26dd95087fec2dff10eb457df56001bbcde441b8
deriando <10672809+deriando@users.noreply.github.com>
2025-03-17 01:16:27 +0800

    fix db scripts

--- apps/web/drizzle.config.ts
+++ apps/web/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle_output",
-  schema: "./src/lib/db/schema.ts",
+  schema: "./src/libs/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DB_CONNECTION_STRING!,
