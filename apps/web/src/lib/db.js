247e5d792a805c8ae9559cd40f148aba8fa42dac
Deriando <10672809+deriando@users.noreply.github.com>
2025-03-17 00:30:44 +0800

    Delete apps/web/src/lib/db.js

--- apps/web/src/lib/db.js
+++
@@ -1,8 +0,0 @@
-import { Pool } from 'pg';
-
-const pool = new Pool({
-  connectionString: process.env.DB_CONNECTION_STRING,
-  ssl: { rejectUnauthorized: false } // Required for Neon DB
-});
-
-export default pool;
