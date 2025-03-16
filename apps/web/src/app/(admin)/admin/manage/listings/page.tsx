34a93bd65586381e0fe41932fd3b2d562fd14917
deriando <10672809+deriando@users.noreply.github.com>
2025-03-17 00:50:20 +0800

    view users under admin page - view only, manage WIP

--- apps/web/src/app/(admin)/admin/manage/listings/page.tsx
+++ apps/web/src/app/(admin)/admin/manage/listings/page.tsx
@@ -1,6 +1,9 @@
 import { Container } from "@mui/material";
+import { connection } from "next/server";
 
 export default async function ManageListingsPage() {
+  await connection();
+
   return (
     <Container>
       <h1>Listings</h1>
