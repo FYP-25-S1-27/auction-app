d9f816ae01546c0b4278093ae889593a179044b4
deriando <10672809+deriando@users.noreply.github.com>
2025-04-17 23:04:17 +0800

    redirect to /search page on search submit + search button

--- apps/web/src/libs/components/NavBar.tsx
+++ apps/web/src/libs/components/NavBar.tsx
@@ -1,120 +1,3 @@
-// "use client";
-// import Image from "next/image";
-// import logo from "@public/logo.svg";
-// import NextLink from "next/link";
-// import {
-//   TextField,
-//   Box,
-//   Link,
-//   InputAdornment,
-//   Stack,
-//   Container,
-//   Avatar,
-//   IconButton,
-//   Menu,
-//   MenuItem,
-// } from "@mui/material";
-// import { useUser } from "@auth0/nextjs-auth0";
-// import Search from "@mui/icons-material/Search";
-// import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
-// import { Fragment, useEffect, useState } from "react";
-// import { getRole } from "./action";
-
-// export default function NavBar() {
-//   const [is_admin, setis_admin] = useState<boolean>(false);
-
-//   const auth = useUser();
-//   useEffect(() => {
-//     if (auth.user) {
-//       getRole(auth.user.sub).then((x) => {
-//         if (x[0].is_admin) {
-//           setis_admin(true);
-//         } else {
-//           setis_admin(false);
-//         }
-//       });
-//     }
-//   }, [auth.user]);
-
-//   return (
-//     <Container>
-//       <Stack
-//         marginTop={1}
-//         direction="row"
-//         alignItems="center"
-//         justifyContent="center"
-//         alignContent="center"
-//         alignSelf="center"
-//       >
-//         <NextLink href="/">
-//           <Image src={logo} alt="GoGavel logo" width={150} />
-//         </NextLink>
-//         <TextField
-//           label="Search"
-//           variant="outlined"
-//           slotProps={{
-//             input: {
-//               startAdornment: (
-//                 <InputAdornment position="end">
-//                   <Search />
-//                 </InputAdornment>
-//               ),
-//             },
-//           }}
-//           fullWidth
-//           sx={{ marginLeft: 2, marginRight: 2 }}
-//         />
-//         {auth.isLoading ? (
-//           <Box component={"div"} sx={{ display: "flex", gap: 2 }}>
-//             <Link href="/auth/login?screen_hint=signup">Register</Link>{" "}
-//             {/* to be skeleton*/}
-//             <Link href="/auth/login">Login</Link>
-//           </Box>
-//         ) : auth.user ? (
-//           // <Avatar src={auth.user.picture} alt={auth.user.nickname} />
-//           <PopupState variant="popover" popupId="avatar-menu">
-//             {(popupState) => (
-//               <Fragment>
-//                 {/* To create Listing page */}
-//                 <div style={{ display: "flex", alignItems: "center", gap: "16px" }}>
-//                   <NextLink href="/createlisting" passHref>
-//                     <span style={{ cursor: "pointer", fontSize: "16px", fontWeight: "bold", color: "#333" }}>Sell</span>
-//                   </NextLink>
-//                 </div>
-
-//                 <IconButton {...bindTrigger(popupState)}>
-//                   <Avatar
-//                     {...bindTrigger(popupState)}
-//                     src={auth.user.picture}
-//                     alt={auth.user.nickname}
-//                   />
-//                 </IconButton>
-//                 <Menu {...bindMenu(popupState)}>
-//                   {is_admin && (
-//                     <MenuItem onClick={popupState.close}>
-//                       <NextLink href="/admin">Admin Panel</NextLink>
-//                     </MenuItem>
-//                   )}
-//                   <MenuItem onClick={popupState.close}>Profile</MenuItem>
-//                   <MenuItem onClick={popupState.close}>My account</MenuItem>
-//                   <MenuItem onClick={popupState.close}>
-//                     <NextLink href="/auth/logout">Logout</NextLink>
-//                   </MenuItem>
-//                 </Menu>
-//               </Fragment>
-//             )}
-//           </PopupState>
-//         ) : (
-//           <Box component={"div"} sx={{ display: "flex", gap: 2 }}>
-//             <Link href="/auth/login?screen_hint=signup">Register</Link>
-//             <Link href="/auth/login">Login</Link>
-//           </Box>
-//         )}
-//       </Stack>
-//     </Container>
-//   );
-// }
-
 "use client";
 import Image from "next/image";
 import logo from "@public/logo.svg";
@@ -123,7 +6,6 @@ import {
   TextField,
   Box,
   Link,
-  InputAdornment,
   Container,
   Avatar,
   IconButton,
@@ -131,22 +13,28 @@ import {
   MenuItem,
   AppBar,
   Toolbar,
+  Button,
   // Typography,
 } from "@mui/material";
 import { useUser } from "@auth0/nextjs-auth0";
-import Search from "@mui/icons-material/Search";
 import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
 import { Fragment, useEffect, useState } from "react";
 import { getRole } from "@/libs/actions/db/users";
+import { useRouter } from "next/navigation";
 // import { getListingCategories } from "../actions/db/listing_category";
 // import { listing_category } from "../db/schema";
 
 export default function NavBar() {
-  const [is_admin, setis_admin] = useState<boolean>(false);
-  // const [categories, setCategories] = useState<
-  //   (typeof listing_category.$inferSelect)[]
-  // >([]);
   const auth = useUser();
+  const [is_admin, setis_admin] = useState<boolean>(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const router = useRouter();
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (searchQuery.trim()) {
+      router.push(`/search?name=${searchQuery}`);
+    }
+  };
 
   useEffect(() => {
     if (auth.user) {
@@ -158,11 +46,6 @@ export default function NavBar() {
         }
       });
     }
-
-    // getListingCategories().then((x) => {
-    //   // append the categories to the state
-    //   setCategories(x);
-    // });
   }, [auth.user]);
 
   return (
@@ -180,41 +63,28 @@ export default function NavBar() {
           <NextLink href="/">
             <Image src={logo} alt="GoGavel logo" width={150} />
           </NextLink>
-          {/* <Box sx={{ marginLeft: 2 }}>
-            <PopupState variant="popover" popupId="categories-menu">
-              {(popupState) => (
-                <Fragment>
-                  <IconButton {...bindTrigger(popupState)}>
-                    <Typography>Categories</Typography>
-                  </IconButton>
-                  <Menu {...bindMenu(popupState)}>
-                    {categories.map((category) => (
-                      <MenuItem key={category.name} onClick={popupState.close}>
-                        <NextLink href={`/category/${category.name}`} passHref>
-                          <span style={{ color: "#333" }}>{category.name}</span>
-                        </NextLink>
-                      </MenuItem>
-                    ))}
-                  </Menu>
-                </Fragment>
-              )}
-            </PopupState>
-          </Box> */}
-          <TextField
-            label="Search"
-            variant="outlined"
-            slotProps={{
-              input: {
-                startAdornment: (
-                  <InputAdornment position="end">
-                    <Search />
-                  </InputAdornment>
-                ),
-              },
+          <Box
+            component="form"
+            onSubmit={handleSearch}
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              flexGrow: 1,
+              mx: 2,
+              gap: 1,
             }}
-            fullWidth
-            sx={{ marginLeft: 2, marginRight: 2 }}
-          />
+          >
+            <TextField
+              label="Search"
+              variant="outlined"
+              fullWidth
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />{" "}
+            <Button type="submit" variant="contained" color="primary">
+              Search
+            </Button>
+          </Box>
           {auth.isLoading ? (
             <Box component={"div"} sx={{ display: "flex", gap: 2 }}>
               <Link href="/auth/login?screen_hint=signup">Register</Link>
