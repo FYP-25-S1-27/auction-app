888fa69afc1feb34689dc1fc36152e909ff0163c
deriando <10672809+deriando@users.noreply.github.com>
2025-05-09 00:33:17 +0800

    change tab-based carousel to arrow-based, delete no longer used Toppicks.tsx and YouMightAlsoLike.tsx - replaced by ListingCarousel.tsx

--- apps/web/src/app/(user)/page.tsx
+++ apps/web/src/app/(user)/page.tsx
@@ -1,18 +1,68 @@
+"use client";
+
 import { Container } from "@mui/material";
 import CategoryBar from "@/libs/components/CategoryBar";
-import Toppicks from "@/libs/components/Toppicks";
 import EndingSoon from "@/libs/components/Endingsoon";
 import PopularCategories from "@/libs/components/PopularCategories";
-import YouMightAlsoLike from "@/libs/components/YouMightAlsoLike";
+import { useEffect, useState } from "react";
+import { TopListingsInCategory } from "../api/toppicks/route";
+import ListingCarousel from "@/libs/components/listings/ListingCarousel";
+import { listings } from "@/libs/db/schema";
 
-export default async function LandingPage() {
+export default function LandingPage() {
+  const [topPicksListings, setTopPicksListings] =
+    useState<TopListingsInCategory>([]);
+
+  const [recommendedItems, setRecommendedItems] = useState<
+    (typeof listings.$inferSelect)[]
+  >([]);
+
+  async function fetchTopPicks() {
+    const response = await fetch("/api/toppicks");
+    if (!response.ok) {
+      throw new Error("Failed to fetch top picks");
+    }
+    const data = await response.json();
+    setTopPicksListings(data);
+  }
+
+  async function fetchRecommendedItems() {
+    try {
+      const response = await fetch("/api/recommended_items");
+      if (!response.ok) {
+        throw new Error("Failed to fetch recommended items");
+      }
+      const data = await response.json();
+      setRecommendedItems(data);
+    } catch (error) {
+      console.error("Error fetching recommended items:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchTopPicks().catch((error) => {
+      console.error("Error fetching top picks:", error);
+    });
+
+    fetchRecommendedItems().catch((error) => {
+      console.error("Error fetching recommended items:", error);
+    });
+  }, []);
   return (
     <Container sx={{ minHeight: "100vh" }}>
       <CategoryBar />
-      <Toppicks />
+      {/* Top Picks in each category */}
+      <ListingCarousel
+        listings={topPicksListings}
+        title="Top picks in each collection"
+      />
       <EndingSoon />
       <PopularCategories />
-      <YouMightAlsoLike />
+      {/* Recommended items / You might also like */}
+      <ListingCarousel
+        listings={recommendedItems}
+        title="You might also like"
+      />
     </Container>
   );
 }
