baaa9f1f6a403a317a43f2c34b4b3ee1f7091340
deriando <10672809+deriando@users.noreply.github.com>
2025-05-11 13:29:15 +0800

    top picks and recommended items now shows on-going auctions only

--- apps/web/src/libs/actions/db/listings/recommended.ts
+++ apps/web/src/libs/actions/db/listings/recommended.ts
@@ -2,12 +2,13 @@
 
 import { db } from "@/libs/db/drizzle";
 import { listings } from "@/libs/db/schema";
-import { sql } from "drizzle-orm";
+import { gt, sql } from "drizzle-orm";
 
 export default async function getRecommendedListings() {
   const recommendedItems = await db
     .select()
     .from(listings)
+    .where(gt(listings.endTime, new Date().toISOString()))
     .orderBy(sql`RANDOM()`)
     .limit(20);
   return recommendedItems;
