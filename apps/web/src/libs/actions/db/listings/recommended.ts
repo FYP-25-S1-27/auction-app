57ca8e911c4fd153cfc6b1809098e6d69944e83e
deriando <10672809+deriando@users.noreply.github.com>
2025-05-13 00:21:47 +0800

    recommendation is now based on user interests - fall back to random

--- apps/web/src/libs/actions/db/listings/recommended.ts
+++ apps/web/src/libs/actions/db/listings/recommended.ts
@@ -2,19 +2,41 @@
 
 import { db } from "@/libs/db/drizzle";
 import { listings } from "@/libs/db/schema";
-import { and, eq, gt, sql } from "drizzle-orm";
+import { and, eq, gt, inArray, sql } from "drizzle-orm";
+import { getUserInterests } from "../userCategoryInterests";
 
-export default async function getRecommendedListings() {
-  const recommendedItems = await db
+export default async function getRecommendedListings(
+  userInterests?: Awaited<ReturnType<typeof getUserInterests>>
+) {
+  const baseConditions = and(
+    gt(listings.endTime, new Date().toISOString()),
+    eq(listings.status, "ACTIVE")
+  );
+  // Only apply category filter if user has interests
+  const whereCondition =
+    userInterests && userInterests.length > 0
+      ? and(
+          baseConditions,
+          inArray(
+            listings.category,
+            userInterests.map((interest) => interest.categoryName)
+          )
+        )
+      : baseConditions;
+  let recommendedItems = await db
     .select()
     .from(listings)
-    .where(
-      and(
-        gt(listings.endTime, new Date().toISOString()),
-        eq(listings.status, "ACTIVE")
-      )
-    )
+    .where(whereCondition)
     .orderBy(sql`RANDOM()`)
     .limit(20);
+  if (recommendedItems.length === 0) {
+    // If no recommended items, fallback to all active listings
+    recommendedItems = await db
+      .select()
+      .from(listings)
+      .where(baseConditions)
+      .orderBy(sql`RANDOM()`)
+      .limit(20);
+  }
   return recommendedItems;
 }
